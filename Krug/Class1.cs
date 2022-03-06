1469eb2cf963a4b7fbe2d8869d16ffcfb1672212
Кристина Андреева <kandreeva952@gmail.com>
2022-03-06 21:06:11 +0300

    Добавьте файлы проекта.

---
+++ Krug/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krug
+{
+    class Krug
+    {
+        double plo;
+        double radius;
+
+        public Krug()
+        {
+            plo = 0;
+            radius = 0;
+        }
+
+        public Krug(double plo, double radius)
+        {
+            this.plo = plo;
+            this.radius = radius;
+        }
+        public double Plo
+        {
+            get
+            {
+                return plo;
+            }
+            set
+            {
+                if (value<= 0)
+                    throw new Exception("Ошибка!");
+                else
+                    plo = value;
+
+            }
+        }
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (value< 0)
+                    throw new Exception("Ошибка!");
+                else
+                    radius = value;
+
+            }
+        }
+        public double RasPlo(double radius)
+        {
+            plo = Math.Round( Math.Pow(radius,2) * Math.PI);
+            return plo;
+        }
+
+        }
+    }
+
---
+++ Krug/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Krug krug = new Krug();
+                double r = krug.Radius;
+                r= Double.Parse(textBox1.Text);
+                double b = krug.RasPlo(r);
+                textBox2.Text =""+ b ;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Введите значения заново!");
+               
+            }
+        }
+    }
+}
